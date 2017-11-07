e621f40247cbdb64f90a778a3412c48f6197607d
Shunsuke 'bep' Miyazaki <beplocks661@gmail.com>
2017-11-08 00:40:13 +0900

    Lesson-1 7/12

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top' => 'home#top'
+  # get 'home/top' => 'home#top'
+  get 'top' => 'home#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
